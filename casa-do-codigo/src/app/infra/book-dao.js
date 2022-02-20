c54196e4a802fcd6b9ab999989a2ecc8a4510cee
jmcdutra <jmcamposdutra@gmail.com>
2022-02-20 02:55:35 -0300

    finalização

--- casa-do-codigo/src/app/infra/book-dao.js
+++ casa-do-codigo/src/app/infra/book-dao.js
@@ -3,13 +3,49 @@ class BookDataAccessObject {
         this._db = db
     }
 
+    searchByID(ID) {
+        return new Promise((resolve, reject) => {
+            this._db.get("SELECT * FROM livros WHERE id = ?", [ID], (err, book) => {
+                if (err) return reject("Não foi possível encontrar o livro com o ID " + ID)
+
+                return resolve(book)
+            })
+        })
+    }
+
+    update(book) {
+        return new Promise((resolve, reject) => {
+            this._db.run("UPDATE livros SET titulo = ?, preco = ?, descricao = ? WHERE id = ?", [book.titulo, book.preco, book.descricao, book.id], (err) => {
+                if (err) return reject("Não foi possível atualizar o livro!")
+
+                resolve();
+            })
+        })
+    }
+
+    remove(id) {
+        return new Promise((resolve, reject) => {
+            this._db.run("DELETE FROM livros WHERE id = ?", [id], (err) => {
+                if (err) return reject("Não foi possível deletar o livro!")
+
+                resolve()
+            })
+        })
+    }
+
+    add(book) {
+        return new Promise((resolve, reject) => {
+            this._db.run("INSERT INTO livros (titulo, preco, descricao) values (?, ?, ?)", [book.titulo, book.preco, book.descricao], (err) => {
+                if (err) console.log(err); return reject("Não foi possível adicionar o livro.")
+                resolve();
+            })
+        })
+    }
+
+
     list() {
         return new Promise((resolve, reject) => {
-            this._db.all("SELECT * FROM livros", (error, result) => {
-                if (error) reject("Não foi possível listar os livros!");
-
-                return resolve(result);
-            })
+            this._db.all("SELECT * FROM livros", (error, result) => { if (error) reject("Não foi possível listar os livros!"); return resolve(result) })
         })
     }
 }
--- casa-do-codigo/src/app/routes/routes.js
+++ casa-do-codigo/src/app/routes/routes.js
@@ -16,10 +16,40 @@ module.exports = (app) => {
         </html>`)
     })
 
-    app.get('/livros', (request, response) => {
-
+    app.get('/livros', (_, response) => {
         const books = new BookDataAccessObject(db);
         books.list().then(_books => response.marko(require("../views/livros/lista/lista.marko"), { livros: _books })).catch(err => {throw new Error(err)})
+    })
 
+    app.get('/livros/form', (_, response) => {
+        response.marko(require("../views/livros/form/form.marko"), { livro: {} })
+    })
+
+    app.post("/livros", (request, response) => {
+        const books = new BookDataAccessObject(db);
+
+        books.add(request.body).then(response.redirect("/livros")).catch(err => console.log(err))
+    })
+
+    app.put("/livros", (request, response) => {
+        const books = new BookDataAccessObject(db);
+
+        books.update(request.body).then(response.redirect("/livros")).catch(err => console.log(err))
+    })
+
+    app.delete("/livros/:id", (request, response) => {
+        const id = request.params.id;
+        const books = new BookDataAccessObject(db);
+
+        books.remove(id).then(() => response.status(200).end()).catch(err => console.log(err))
+    })
+
+    app.get("/livros/form/:id", (request, response) => {
+        const id = request.params.id;
+        const books = new BookDataAccessObject(db);
+
+        console.log(id)
+
+        books.searchByID(id).then(book => response.marko(require("../views/livros/form/form.marko"), {livro: book})).catch(err => console.log(err))
     })
 } 
